3ed5d7180b12e9f7e93312eaaf3c933f418db6e9
Admin <Admin@DESKTOP-7D93OID>
2024-06-05 06:35:46 +0500

    Add project files.

---
+++ GYM Management/Views/Home/Exercise.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GYM_Management.Views.Home
+{
+    public class ExerciseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
